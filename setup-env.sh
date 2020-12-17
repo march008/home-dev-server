4bce6b03aefcff41355d3ceb4fd4f68dbc64e4b9
march008 <mvargas911@gmail.com>
2020-12-17 15:49:30 +0100

    adding files

---
+++ setup-env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p ./data/nginx/{conf,html,vhost,certs,dhparam}
+mkdir -p ./data/portainer/data
+mkdir -p ./data/jenkins/data
+mkdir -p ./data/reposilite/data
+mkdir -p ./data/gitlab/{config,data,logs}
+mkdir -p ./data/openproject/{pgata,assets}
+mkdir -p ./data/db/{mysql,postgre}
+mkdir -p ./data/mailu
+
+docker network create homenet
