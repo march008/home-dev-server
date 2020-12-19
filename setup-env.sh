149e6a48186df65a330861810f97480b8fe11436
march008 <mvargas911@gmail.com>
2020-12-19 22:02:20 +0100

    redmine mount themes

--- setup-env.sh
+++ setup-env.sh
@@ -5,7 +5,7 @@ mkdir -p ./data/portainer/data
 mkdir -p ./data/jenkins/data
 mkdir -p ./data/reposilite/data
 mkdir -p ./data/gitlab/{config,data,logs}
-mkdir -p ./data/redmine/{data,db}
+mkdir -p ./data/redmine/{data,db,themes}
 mkdir -p ./data/db/{mysql,postgre}
 mkdir -p ./data/mailu
 
