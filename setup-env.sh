b65bec22be4b6641bc708a8158275df9c2278244
march008 <mvargas911@gmail.com>
2020-12-19 20:52:40 +0100

    +redmine

--- setup-env.sh
+++ setup-env.sh
@@ -5,8 +5,10 @@ mkdir -p ./data/portainer/data
 mkdir -p ./data/jenkins/data
 mkdir -p ./data/reposilite/data
 mkdir -p ./data/gitlab/{config,data,logs}
-mkdir -p ./data/openproject/{pgata,assets}
+mkdir -p ./data/redmine/{data,db}
 mkdir -p ./data/db/{mysql,postgre}
 mkdir -p ./data/mailu
+
+chmod +777 ./data/jenkins/data
 
 docker network create homenet
