845d1c8f0b952f548b8f2d53bd41fd804e371f5c
march008 <mvargas911@gmail.com>
2020-12-21 12:16:56 +0100

    plugins themes

--- redmine.sh
+++ redmine.sh
@@ -7,3 +7,17 @@ unzip "*.zip" -d ./data/redmine/themes/
 #redmine plugins
 cd ../plugins
 unzip "*.zip" -d ./data/redmine/plugins/
+
+
+
+#installing plugins
+sudo docker exec -it redmine bash
+#in container
+cd /usr/src/redmine
+bundle install --without development test --no-deployment
+
+bundle exec rake redmine:plugins NAME=redmine_agile RAILS_ENV=production
+bundle exec rake redmine:plugins NAME=redmine_tags RAILS_ENV=production
+bundle exec rake redmine:plugins NAME=redmine_contacts RAILS_ENV=production
+bundle exec rake redmine:plugins NAME=redmine_crm RAILS_ENV=production
+
