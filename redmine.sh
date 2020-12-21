6e630e7bc07fec4c0749bcfb9e7b5125a71b0e0e
march008 <mvargas911@gmail.com>
2020-12-21 12:40:51 +0100

    container commands

--- redmine.sh
+++ redmine.sh
@@ -11,13 +11,11 @@ unzip "*.zip" -d ./data/redmine/plugins/
 
 
 #installing plugins
-sudo docker exec -it redmine bash
-#in container
-cd /usr/src/redmine
-bundle install --without development test --no-deployment
-
-bundle exec rake redmine:plugins NAME=redmine_agile RAILS_ENV=production
-bundle exec rake redmine:plugins NAME=redmine_tags RAILS_ENV=production
-bundle exec rake redmine:plugins NAME=redmine_contacts RAILS_ENV=production
-bundle exec rake redmine:plugins NAME=redmine_crm RAILS_ENV=production
+sudo docker exec -it redmine bash -c \
+"cd /usr/src/redmine;
+bundle install --without development test --no-deployment;
+bundle exec rake redmine:plugins NAME=redmine_agile RAILS_ENV=production;
+bundle exec rake redmine:plugins NAME=redmine_tags RAILS_ENV=production;
+bundle exec rake redmine:plugins NAME=redmine_contacts RAILS_ENV=production;
+bundle exec rake redmine:plugins NAME=redmine_crm RAILS_ENV=production;"
 
