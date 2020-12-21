46b33543fe103d920a6ca181f7fac0af0388c832
march008 <mvargas911@gmail.com>
2020-12-21 12:46:57 +0100

    plugin paths updatae

--- redmine.sh
+++ redmine.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #redmine themes
-cd ./themes
-unzip "*.zip" -d ./data/redmine/themes/
+cd ./themes/
+unzip "*.zip" -d ../data/redmine/themes/
 
 #redmine plugins
-cd ../plugins
-unzip "*.zip" -d ./data/redmine/plugins/
+cd ../plugins/
+unzip ".*.zip" -d ../data/redmine/plugins/
 
 
 
