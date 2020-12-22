796a4ead51adb33d6b28cb459d67c4bde4bfd27d
march008 <mvargas911@gmail.com>
2020-12-22 16:21:33 +0100

    youtrack

--- setup-env.sh
+++ setup-env.sh
@@ -4,14 +4,15 @@ mkdir -p ./data/nginx-manager/{letsencrypt,data,db}
 mkdir -p ./data/portainer/data
 mkdir -p ./data/jenkins/data
 mkdir -p ./data/reposilite/data
-mkdir -p ./data/gitlab/{config,data,logs}
+# mkdir -p ./data/gitlab/{config,data,logs}
 mkdir -p ./data/gitea/{data,db}
-mkdir -p ./data/redmine/{data,db,themes,plugins}
+# mkdir -p ./data/redmine/{data,db,themes,plugins}
 mkdir -p ./data/db/{mysql,postgre}
-mkdir -p ./data/mailu
+# mkdir -p ./data/mailu
+mkdir -p ./data/youtrack{data,confs,backups,logs}
 
 chmod +777 ./data/jenkins/data
 
 docker network create homenet
 
-sudo apt install -y zip unzip
+# sudo apt install -y zip unzip
